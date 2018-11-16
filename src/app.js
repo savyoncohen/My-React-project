7a5df9988be74734767660884e01ecb15aa3cb18
savyoncohen <savyon.cohen@gmail.com>
2018-11-16 17:30:22 +0200

    Styling the app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,52 @@
-// import subtract, { square, add } from './utils.js';
-// console.log("app.js is running");
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81));
 
-// import isSenior, { isAdult, canDrink } from './person.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css'
+import './styles/styles.scss';
 
-// console.log(isAdult(17));
-// console.log(canDrink(21));
-// console.log(isSenior(64));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+
+// class Oldsyntax {
+//   constructor() {
+//     this.name = 'Mike';
+//     this.getGreeting = this.getGreeting.bind(this);
+//   }
+
+//   getGreeting() {
+//     return `Hi. my name is ${this.name}.`;
+//   }
+// }
+
+// const oldSyntax = new Oldsyntax();
+// console.log(oldSyntax);
+// console.log(oldSyntax.getGreeting());
+
+// // new syntax
+
+// class NewSyntax {
+//   name = 'Jen';
+
+//   getGreeting = () => {
+//     return `Hi. my name is ${this.name}.`;
+//   }
+// }
+
+// const newSyntax = new NewSyntax();
+// console.log(newSyntax);
+// console.log(newSyntax.getGreeting());
+
+
+// Grab the add function from the add.js file in the utils folder
+// Grab React from the react npm module
+// add(2,4)
+
+// const User = (props) => {
+//   return (
+//     <div>
+//       <p>Name: {props.name}</p>
+//       <p>Age: {props.age}</p>
+//     </div>
+//   );
+// };
